d2362eddfed31e12f9caf0312bab0124fe74ca07
Maka <horak.maksym@gmail.com>
2017-11-21 18:09:28 +0200

    add map button

--- SushiTime/Controllers/WebSiteTableViewController.swift
+++ SushiTime/Controllers/WebSiteTableViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SafariServices
+import MapKit
 
 class WebSiteTableViewController: UIViewController {
 
@@ -22,7 +23,26 @@ class WebSiteTableViewController: UIViewController {
         // Dispose of any resources that can be recreated.
         //http://www.sushitime.lviv.ua
     }
- 
+    
+    @IBAction func ShowWhere(_ sender: Any) {
+    
+    
+    let latitude:CLLocationDegrees = 49.862455
+    let longitude:CLLocationDegrees = 24.019259
+    
+    let regionDistance:CLLocationDistance = 100;
+    let coordinates = CLLocationCoordinate2DMake(latitude, longitude)
+    let regionSpan = MKCoordinateRegionMakeWithDistance(coordinates, regionDistance, regionDistance)
+        
+        let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)]
+        
+        let placemark = MKPlacemark(coordinate: coordinates)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = "Sushi Time"
+        mapItem.openInMaps(launchOptions: options)
+        
+    }
+ //49.862455, 24.019259
     
     @IBAction func openWebSite(_ sender: Any) {
         let svc = SFSafariViewController(url: URL(string: "http://www.sushitime.lviv.ua/men")!)
