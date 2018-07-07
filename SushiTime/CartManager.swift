4c4a93373405554b595611a6ce312dbc4000d58e
Maka <horak.maksym@gmail.com>
2018-07-07 18:55:39 +0300

    add sreet and house in order

--- SushiTime/CartManager.swift
+++ SushiTime/CartManager.swift
@@ -17,17 +17,15 @@ class CartManager: NSObject {
         return cartItems.compactMap({ $0.count * $0.priceName }).reduce(0,+)
     }
     
-    func formText(phoneNumber: String, email: String?, name: String) -> String {
+    func formText(phoneNumber: String, email: String?, name: String, street: String, house: String) -> String {
         var html = """
         <table border="1" width="30%" height="100">
         <tr>
         <td colspan="3" align="center" >Sushi Time Доставка</td>
         </tr>
-        
         <tr>
         <td>Ім'я:</td>       <td colspan="2" >\(name)</td>
         </tr>
-
         <tr>
         <td>Номер телефону:</td>       <td colspan="2" >\(phoneNumber)</td>
         </tr>
@@ -42,6 +40,12 @@ class CartManager: NSObject {
         }
         html +=
         """
+        <tr>
+        <td>Вулиця:</td>       <td colspan="2" >\(street)</td>
+        </tr>
+        <tr>
+        <td>Будинок:</td>       <td colspan="2" >\(house)</td>
+        </tr>
         <tr>
         <td>Name:</td>           <td>Quantity:</td>            <td>Total:</td>
         </tr>
