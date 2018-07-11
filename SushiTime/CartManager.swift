7004133f0381f8336450305e1edc1e2283c9b3b3
Maka <horak.maksym@gmail.com>
2018-07-11 17:50:01 +0300

    change images size

--- SushiTime/CartManager.swift
+++ SushiTime/CartManager.swift
@@ -19,41 +19,22 @@ class CartManager: NSObject {
     
     func formText(phoneNumber: String, email: String?, name: String, street: String, house: String, enter: String?, level: String?) -> String {
         var html = """
-        <table border="1" width="30%" height="100">
-        <tr>
-        <td colspan="3" align="center" >Sushi Time Доставка</td>
-        </tr>
-        <tr>
-        <td>Ім'я:</td>       <td colspan="2" >\(name)</td>
-        </tr>
-        <tr>
-        <td>Номер телефону:</td>       <td colspan="2" >\(phoneNumber)</td>
-        </tr>
+        <html><table border="1" width="400"><tr><td colspan="3" align="center">Sushi Time Доставка</td></tr><tr><td>Ім'я:</td><td colspan="2">\(name)</td></tr><tr><td>Номер телефону:</td><td colspan="2" >\(phoneNumber)</td></tr>
         """
         if let email = email {
             html +=
             """
-            <tr>
-            <td>Email:</td>       <td colspan="2" >\(email)</td>
-            </tr>
+            <tr><td>Email:</td><td colspan="2">\(email)</td></tr>
             """
         }
         html +=
         """
-        <tr>
-        <td>Вулиця:</td>       <td colspan="2" >\(street)</td>
-        </tr>
-        <tr>
-        <td>Будинок:</td>       <td colspan="2" >\(house)</td>
-        </tr>
-        <tr>
+        <tr><td>Вулиця:</td><td colspan="2">\(street)</td></tr><tr><td>Будинок:</td><td colspan="2">\(house)</td></tr><tr>
         """
         if let enter = enter {
             html +=
             """
-            <tr>
-            <td>Під'їзд:</td>       <td colspan="2" >\(enter)</td>
-            </tr>
+            <tr><td>Під'їзд:</td><td colspan="2">\(enter)</td></tr>
             """
         }
         html +=
@@ -62,27 +43,20 @@ class CartManager: NSObject {
         if let level = level {
             html +=
             """
-            <tr>
-            <td>Поверх:</td>       <td colspan="2" >\(level)</td>
-            </tr>
+            <tr><td>Поверх:</td><td colspan="2">\(level)</td></tr>
             """
         }
         html +=
         """
-        <td>Name:</td>           <td>Quantity:</td>            <td>Total:</td>
-        </tr>
-
+        <td>Name:</td><td>Quantity:</td><td>Total:</td></tr>
         """
         html = html + cartItems.compactMap({ item in
             """
-            <tr>
-            <td>\(item.name)</td>          <td>\(item.count)</td>           <td>\(item.count * item.priceName)</td>
-            </tr>
+            <tr><td>\(item.name)</td><td>\(item.count)</td><td>\(item.count * item.priceName)</td></tr>
             """
         }).reduce("", +)
-        html += "<td>Total:</td>       <td ></td>   <td >\(self.calculateOveral())</td>"
-        html += "</table>"
-    
+        html += "<td>Total:</td><td></td><td>\(self.calculateOveral())</td>"
+        html += "</table></html>"
         return html
     }
 }
